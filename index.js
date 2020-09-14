b92652efa15eb1ae3fec3984dad65656e069e029
tarun <tarunmeena512@gmail.com>
2020-09-14 14:53:25 +0530

    Adding Cookie support.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 var bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
-const assert = require('assert');
-const Dishes = require('./models/dishes')
 const dishRouter = require('./routers/dishRouter')
 const promotionRouter = require('./routers/promoRouter')
 const leaderRouter = require('./routers/leaderRouter');
 const { Buffer } = require('buffer');
-
+const CookieParser = require('cookie-parser');
 const url = 'mongodb://127.0.0.1:27017/confusion';
 const connect = mongoose.connect(url);
 
@@ -20,28 +17,45 @@ connect.then(db => {
 
 const app = express()
 const port = 3000
+//adding cookie
+app.use(CookieParser('12345-12345-12345-12345'));
 
 //Basic auhentication before allow routes
 function auth(req, res, next) {
   console.log(req.headers);
-  var authHeaders = req.headers.authorization;
-  if (!authHeaders) {
-    var error = new Error('Your Are Not Authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic')
-    res.status = 401;
-    next(error)
-    return;
+  console.log(req.signedCookies);
+
+  if (!req.signedCookies.user) {
+    var authHeaders = req.headers.authorization;
+
+    if (!authHeaders) {
+      var error = new Error('Your Are Not Authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic')
+      res.status = 401;
+      next(error)
+      return;
+    }
+
+    var auth = new Buffer.from(authHeaders.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user === 'admin' && pass === 'admin') {
+      res.cookie('user', 'admin', { signed: true })
+      next() //authorized
+    } else {
+      var error = new Error('Your Are Not Authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic')
+      res.status = 401;
+      next(error);
+    }
   }
-  var auth = new Buffer.from(authHeaders.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user === 'admin' && pass === 'admin') {
-    next() //authorized
+  else if (req.signedCookies.user === "admin") {
+    next();
   } else {
     var error = new Error('Your Are Not Authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic')
     res.status = 401;
-    next(error);
+    return; next(error)
+
   }
 }
 app.use(auth);
