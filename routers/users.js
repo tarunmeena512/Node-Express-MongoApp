0f553b8b23c99e200da89792b55de56773edaa39
tarunmeena512 <30525908+tarunmeena512@users.noreply.github.com>
2020-09-16 15:57:55 +0530

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -1,84 +1,37 @@
 //user registration/login/logout
 const express = require('express')
+var passport = require('passport');
 var bodyParser = require('body-parser')
-const Users = require('../models/user')
-const userRouter = express.Router();
+const User = require('../models/user')
+const router = express.Router();
 
-userRouter.use(bodyParser.json());
+router.use(bodyParser.json());
 
-userRouter.route('/signup').
-    post((req, res, next) => {
-    Users.findOne({ username: req.body.username })
-        .then(user => {
-            if (user != null) {
-                var err = new Error('User already exit!');
-                res.statusCode = 403;
-                next(err);
-            } else {
-                return Users.create({ username: req.body.username, password: req.body.password }).
-                    then(result => {
-                        res.statusCode === 201;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json(result)
-                    }, error => {
-                        next(error);
-                    })
+router.post('/signup', (req, res, next) => {
+    User.register(new User({ username: req.body.username }),
+        req.body.password, (err, user) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({ err: err });
             }
-
-        }, error => { next(error) }).
-        catch(error => {
-            next(error)
-        })
-    })
-
-    userRouter.route('/login').
-    post((req, res, next) => {
-        if (!req.session.user) {
-            var authHeaders = req.headers.authorization;
-
-            if (!authHeaders) {
-              var error = new Error('Your Are Not Authenticated');
-              res.setHeader('WWW-Authenticate', 'Basic')
-              res.statusCode = 401;
-              next(error)
-              return;
+            else {
+                passport.authenticate('local')(req, res, () => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json({ success: true, status: 'Registration Successful!' });
+                });
             }
-        
-            var auth = new Buffer.from(authHeaders.split(' ')[1], 'base64').toString().split(':');
-            var username = auth[0];
-            var password = auth[1];
+        });
+});
 
-            Users.findOne({ username: username }).then(user=>{
-              if(user===null){
-                var error = new Error('User not exit');
-                res.statusCode = 403;
-                next(error)
-                return; 
-              }else if(user.password!==password){
-                var error = new Error('password incorrect');
-                res.statusCode = 403;
-                next(error)
-                return; 
-              }else if(user.username===username && user.password===password){
-                  req.session.user = "authenticate"
-                  res.setHeader('Content-Type', 'text/plain');
-                  res.statusCode = 200;
-                  res.send('authenticate success')
-              }
+router.post('/login', passport.authenticate('local'), (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, status: 'You are successfully logged in!' });
+});
 
-            },error=>{
-                next(error);
-            })
-
-        } else {
-            res.setHeader('Content-Type', 'text/plain');
-           res.statusCode = 200;
-           res.send('You are already authenticated!!')
-        }
-    })
-
-userRouter.route('/logout').
-    get((req, res, next) => {
+router.get('/logout', (req, res) => {
     if (req.session) {
         req.session.destroy();
         res.clearCookie('session-id');
@@ -90,6 +43,5 @@ userRouter.route('/logout').
         next(err);
     }
 })
-
 
 module.exports = userRouter;
