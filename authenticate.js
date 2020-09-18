86521ab3338f83b12e4611deb3ed7bed1319b0f3
tarunmeena512 <30525908+tarunmeena512@users.noreply.github.com>
2020-09-18 18:22:19 +0530

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -4,32 +4,35 @@ var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
-var Jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function (user) {
-    return Jwt.sign(user, config.secretKey, { expiresIn: 3600 });
+exports.getToken = function(user) {
+    return jwt.sign(user, config.secretKey,
+        {expiresIn: 3600});
 };
-
-var opts = {}
+var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    console.log("jwt_payload----" + jwt_payload);
-    User.findOne({ _id: jwt_payload._id }, (err, user) => {
-        if (err) {
-            return done(err, false);
-        } else if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    })
-}))
+exports.jwtPassport = passport.use(new JwtStrategy(opts,
+    (jwt_payload, done) => {
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            else if (user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
 
-exports.verifyUser = passport.authenticate('jwt',{session:false});
+exports.verifyUser = passport.authenticate('jwt', {session: false});
